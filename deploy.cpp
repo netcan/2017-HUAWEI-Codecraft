ff0676be92fd39efb3efb3052b561647eea36bd1
netcan <1469709759@qq.com>
2017-03-26 17:46:37 +0800

    添加Tabu算法，没卵用

--- deploy.cpp
+++ deploy.cpp
@@ -21,18 +21,19 @@ void timeOutHandler(int signo) {
 	return;
 }
 
-int SA(double T = 20.0, double delta = 0.99999) { // 初始温度，迭代系数
+void SA(unordered_set<int>init = {}, double T = 20.0, double delta = 0.99999) { // 模拟退火，初始温度，迭代系数
 	// double T = 20.0, delta = 0.99999; // 初始温度20, 0.999-0.999999
 
 	unordered_set<int> backup, cur;
 
-	for(int u=0; u < mcmf.consumerNum; ++u)
-		backup.insert(mcmf.edges[mcmf.G[u + mcmf.networkNum][0]].to);
+	if(init.empty()) {
+		for(int u=0; u < mcmf.consumerNum; ++u)  // 初始位置
+			backup.insert(mcmf.edges[mcmf.G[u + mcmf.networkNum][0]].to);
+	} else backup = move(init);
 
 	int minCost = MCMF::INF, backCost = MCMF::INF, curCost = MCMF::INF;
 	backCost = mcmf.minCost_Set(backup);
 	minCost = min(minCost, backCost);
-
 
 	int iterationCnt = 0;
 	while(T > 0.1 && runing) {
@@ -87,8 +88,70 @@ int SA(double T = 20.0, double delta = 0.99999) { // 初始温度，迭代系数
 	// puts("\n=====Solution======");
 	// mcmf.showSolution();
 	printf("minCost: %d/%d cdnNum: %ld\n\n", minCost, mcmf.consumerNum * mcmf.costPerCDN, backup.size());
-	return minCost;
 }
+
+
+void Tabu(unordered_set<int>init = {}) { // 禁忌搜索
+	typedef unordered_set<int> X;
+	list<int> H; // 禁忌表，队列
+
+	X x_best;
+	int minCost;
+	X x_now;
+	if(init.empty()) {
+		for(int u=0; u < mcmf.consumerNum; ++u)  // 初始位置
+			x_now.insert(mcmf.edges[mcmf.G[u + mcmf.networkNum][0]].to);
+	} else x_now = move(init);
+
+	pair<int, X> x_next{MCMF::INF, {}}; // 转移
+	H.push_back(minCost = mcmf.minCost_Set(x_now));
+
+	// for(int x: x_now)
+		// printf("%d ", x);
+	// puts("");
+
+	int iterationCnt = 0;
+	while(runing) {
+		int Len = 0;
+		for(int u: x_now) {
+			for(size_t i = 0; i < mcmf.G[u].size(); i+=2) {
+				++Len;
+				int v = mcmf.edges[mcmf.G[u][i]].to; // u->v
+				if(v < mcmf.networkNum) {
+					X tmp{}; // 邻居
+					for(int uu: x_now) {
+						if(uu != u) tmp.insert(uu);
+						else tmp.insert(v);
+					}
+
+					// puts("==============");
+					// for(int x: tmp)
+						// printf("%d ", x);
+					// puts("\n==============");
+
+					int cost = mcmf.minCost_Set(tmp);
+					if(find(H.begin(), H.end(), cost) == H.end() && cost < x_next.first) {
+						x_next.first = cost;
+						x_next.second = move(tmp);
+						if(minCost > cost) {
+							minCost = cost;
+							x_best = x_next.second;
+						}
+					}
+				}
+			}
+		}
+		H.push_back(x_next.first); // 入队
+		x_next.first = MCMF::INF;
+		x_now = move(x_next.second);
+		++iterationCnt;
+		while(H.size() > sqrt(Len)) H.pop_front();
+	}
+
+	mcmf.showSolution();
+	printf("iterationCnt = %d\n", iterationCnt);
+}
+
 
 void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
 {
@@ -97,6 +160,7 @@ void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
 	// 启动计时器
 	alarm(88);
 	mcmf.loadGraph(topo, line_num);
+	// Tabu();
 	SA();
 
 	//- test
