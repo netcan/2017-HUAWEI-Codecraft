aa000fbdbd69dff6bac28abb1ed975fa1d1d42a0
netcan <1469709759@qq.com>
2017-03-27 08:09:02 +0800

    重构基因类型、添加随机数类型

---
+++ gene.h
@@ -0,0 +1,86 @@
+/*************************************************************************
+	> File Name: gene.cpp
+	  > Author: Netcan
+	  > Blog: http://www.netcan666.com
+	  > Mail: 1469709759@qq.com
+	  > Created Time: 2017-03-26 Sun 20:31:48 CST
+ ************************************************************************/
+
+#ifndef __GENE__
+#define __GENE__
+#include <cstdio>
+#include <ctime>
+#include <bitset>
+#include <unordered_set>
+#include "random.h"
+using namespace std;
+
+class Gene {
+	private:
+		const int len; // 长度，0-1200
+		bitset<1024> code;
+	public:
+		Gene(int len): len(len) {
+			discrete_distribution<int> distribution{1, 1};
+			for(int i = 0; i < len; ++i)
+				code[i] = Rand.Random_Int(0, 1);
+		}
+
+		inline void operator*(Gene &b) {
+			int end = Rand.Random_Int(1, len); // 交换的位置，交换一边就行了，因为另一边不动，这里交换左边
+			int begin = Rand.Random_Int(0, end - 1);
+			// int loc = 12;
+			printf("[%d, %d)\n", begin, end);
+			//
+			for(int i = begin; i < end; ++i)
+				if(code[i] != b.code[i]) {
+					code[i] = !code[i];
+					b.code[i] = !b.code[i];
+				}
+		}
+
+		inline void mutation(int loc = -1) { // 突变，[0, len)
+			if(loc == -1) loc = Rand.Random_Int(0, len - 1);
+			else if(loc >= len) return;
+			printf("loc = %d\n", loc);
+			code[loc] = !code[loc];
+		}
+
+		inline void show() {
+			for(int i = 0; i < len; ++i) {
+				if(i != 0 && i % 8 == 0) printf(",");
+				printf(code[i]?"1":"0");
+			}
+			puts("");
+		}
+		inline unordered_set<int> to_Set() {
+			unordered_set<int> S;
+			for(int i = 0; i < len; ++i)
+				if(code[i]) S.insert(i);
+			return S;
+		}
+
+};
+#endif
+
+
+// int main(void) {
+	// Gene ga(10);
+	// Gene gb(10);
+
+	// ga.show();
+	// gb.show();
+	// ga * gb;
+	// ga.show();
+	// gb.show();
+
+	// ga.show();
+	// ga.mutation();
+	// ga.show();
+
+	// for(auto x: ga.to_Set()) {
+		// printf("%d\n", x);
+	// }
+
+	// return 0;
+// }
---
+++ random.h
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: random.cpp
+	  > Author: Netcan
+	  > Blog: http://www.netcan666.com
+	  > Mail: 1469709759@qq.com
+	  > Created Time: 2017-03-27 Mon 07:40:54 CST
+ ************************************************************************/
+
+#ifndef __RANDOM__
+#define __RANDOM__
+#include <cstdio>
+#include <random>
+
+class Random {
+	private:
+		std::default_random_engine generator;
+
+	public:
+		Random(time_t t = time(NULL)): generator(t) {}
+		inline uint32_t Random_Int(uint32_t min, uint32_t max) { // [min, max]
+			return std::uniform_int_distribution<uint32_t>{min, max}(generator);
+		}
+		inline double Random_Real(double min, double max) { // [min, max)
+			return std::uniform_real_distribution<double>{min, max}(generator);
+		}
+};
+Random Rand;
+
+#endif
